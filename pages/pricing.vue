9d97264980349685658e7aa2237dd14199c3383a
Mark Vitin <vitinmaruku@gmail.com>
2026-06-05 15:59:59 +0900

    Revert "Add SAF pricing calculator proxy"
    This reverts commit b5308cdac529f658ef78e970af87fa5b09b54156.

---
+++ pages/pricing.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pricing-page">
+    <PricingHero />
+    <PricingModel />
+    <PricingVolumeModes />
+    <PricingBundles />
+    <PricingModules />
+    <PricingCalculator />
+    <PricingInquiryGuide />
+    <PricingTrust />
+    <PricingFinalCta />
+  </div>
+</template>
